3970a3d03b5babe92e28679189796a907d962338
zszfympx <2792668017@qq.com>
2024-09-21 23:37:50 +0800

    change button style

---
+++ src/components/Count.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+
+var cnt = ref(0);
+
+</script>
+
+<template>
+<button @click="cnt++;"><i class="fa-solid fa-plus"></i></button>
+<p>{{ cnt }}</p>
+<button @click="cnt--"><i class="fa-solid fa-minus"></i></button>
+</template>
+
+<style scoped>
+template{
+    align-items: center;
+}
+button{
+    float: left;
+    height: 24px;
+}
+p{
+    line-height: 12px;
+    height: 12px;
+    float: left;
+}
+</style>
--- src/components/Link.vue
+++ src/components/Link.vue
@@ -1,23 +1,42 @@
 <script setup>
-
+function goto(url) {
+    window.open(url, "_blank");
+}
 </script>
 
 <template>
-<div class>
-    <a href="http://haoz1.mcaltb.top/">Buy</a>
-</div>
-<div>
-    <a href="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=ttEkKhtBjxd183_vapmlTZMHNbvrJ4oG&authKey=0GqhpwLZ4J%2Bp99h%2Blktqfq5lbFQVG9qbGuw1rMO9ehTFw5yjl6JxQ%2BS0qxm6jOza&noverify=0&group_code=995313755">Get Free</a>
-</div>
+<button @click="goto('http://haoz1.mcaltb.top/')">
+    <p>Buy</p>
+</button>
+<button @click="goto('http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=ttEkKhtBjxd183_vapmlTZMHNbvrJ4oG&authKey=0GqhpwLZ4J%2Bp99h%2Blktqfq5lbFQVG9qbGuw1rMO9ehTFw5yjl6JxQ%2BS0qxm6jOza&noverify=0&group_code=995313755')">
+    <p>Get Free</p>
+</button>
 </template>
 
 <style scoped>
-
-div {
-    width: 440px;
-    float: left;
-    border: 5px solid #7fd66e;
-    border-radius: 10px;
+template {
+    display: flex;
+}
+button   {
+  min-width: 450px;
+  height: 40px;
+  color: #fff;
+  padding: 5px 10px;
+  font-weight: bold;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  position: relative;
+  display: flex;
+  outline: none;
+  border-radius: 5px;
+  border: 2px solid #adb5bd;
+  background: #adb5bd;
+  align-items: center;
+  float: left;  
+}
+button:hover {
+  background: #fff;
+  color: #adb5bd
 }
 a {
     text-decoration: none;
