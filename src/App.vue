84e7e1b892ee8e1e581533185df329c2a6bd1f8e
zszfympx <2792668017@qq.com>
2024-09-21 23:50:08 +0800

    add footer

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,24 @@
 import Header from "./components/Header.vue"
 import Link from "./components/Link.vue"
 import Introduction from "./components/Introduction.vue";
+import Footer from "./components/Footer.vue";
+
 </script>
 
 <template>
 <Header></Header>
 <Introduction></Introduction>
 <Link></Link>
+<Footer class="footer"></Footer>   
 </template>
 
 <style scoped>
-
+.footer {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 20px;
+    width: 40px;
+}
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,11 @@
 <script setup>
-
+function goto(url) {
+    window.open(url, "_blank");
+}
 </script>
 
 <template>
-
+<button @click="goto('https://github.com/Sakura-Teams/website')"><i class="fa-brands fa-github"></i></button> 
 </template>
 
 <style scoped>
